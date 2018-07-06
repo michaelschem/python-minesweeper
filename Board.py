6788d7e472ed9774e1df3c703b050c6eec9b8e82
mike <michaelfschem@gmail.com>
2018-07-06 11:04:34 -0700

    change mine generation to use exactly set amount of mines instead of random

--- Board.py
+++ Board.py
@@ -1,10 +1,11 @@
-import random,tkinter,threading
+import random,tkinter,threading,math
 
 from Mine_Button import Mine_Button
 
 class Board:
-	width = 20
-	height = 20
+	width = 36
+	height = 16
+	total_mines = 99
 	board = None
 	ui = None
 	neighbors = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
@@ -15,10 +16,9 @@ class Board:
 		# self.ui_buttons = [[0 for j in range(self.width)] for i in range(self.height)]
 
 	def drop_mines(self):
-		for r_key,row in enumerate(self.board):
-			for c_key,cell in enumerate(row):
-				if random.random() % 10 > 0.9:
-					self.board[r_key][c_key].val = "M"
+		while self.total_mines > 0:
+			self.board[math.floor((random.random() * 10) % self.height)][math.floor((random.random() * 10) % self.width)].val = "M"
+			self.total_mines -= 1
 
 	def place_count(self):
 		for r_key,row in enumerate(self.board):
@@ -31,44 +31,49 @@ class Board:
 		count = 0
 
 		for i in self.neighbors:
-			count += self.has_relative("M",r_key + i[0],c_key + i[1])
+			if self.safe_check("M", r_key + i[0], c_key + i[1]):
+				count += 1
 
 		return count
 
-	def expand_neighbors(self,r_key,c_key):
-		# if not self.board[r_key][c_key].hidden:
-		# 	return
-		if r_key < 0 or c_key < 0:
-			return
-		if c_key > self.width -1 or r_key > self.height -1:
-			return
+	def expand_zeros(self):
+		for r_key,row in enumerate(self.board):
+			for c_key,cell in enumerate(row):
+				if not cell.hidden:
+					for i in self.neighbors:
+						if self.safe_check(0, r_key + i[0], c_key + i[1]) and self.board[r_key + i[0]][c_key + i[1]].hidden:
+							self.board[r_key + i[0]][c_key + i[1]].show()
+							self.expand_zeros()
 
-		cell = self.board[r_key][c_key]
-		if cell.val == 0:
-			cell.show()
+	def expand_frindge(self):
+		positions_to_check = []
+		for r_key,row in enumerate(self.board):
+			for c_key,cell in enumerate(row):
+				if not cell.hidden and not cell.locked:
+					positions_to_check.append({"r":r_key,"c":c_key})
+
+		for position in positions_to_check:
 			for i in self.neighbors:
-				if self.has_relative(0,r_key + i[0],c_key + i[1]) == 1 and self.board[r_key + i[0]][c_key + i[1]].hidden:
-					self.board[r_key + i[0]][c_key + i[1]].show()
-					self.expand_neighbors(r_key + i[0],c_key + i[1])
-				elif i[0] != -1 and i[1] != -1 and r_key + i[0] < self.height and c_key + i[1] < self.width:
-					self.board[r_key + i[0]][c_key + i[1]].show()
-		else:
-			cell.show()
+				if position["r"] + i[0] < self.height and position["c"] + i[1] < self.width and position["r"] + i[0] != -1 and position["c"] + i[1] != -1:
+					self.board[position["r"] + i[0]][position["c"] + i[1]].show()
+					self.board[position["r"] + i[0]][position["c"] + i[1]].locked = True
 
-	def has_relative(self, type, r, c):
+	def safe_check(self, type, r, c):
 		if r < 0 or c < 0:
-			return 0
+			return False
 		if c > self.width -1 or r > self.height -1:
-			return 0
+			return False
 		if self.board[r][c].val == type:
-			return 1
+			return True
 		else:
-			return 0
+			return False
 
 	def click(self, r, c):
-		if self.board[r][c].val == 0:
-			self.expand_neighbors(r,c)
 		self.board[r][c].show()
+		if self.board[r][c].val == 0:
+			self.expand_zeros()
+			self.expand_frindge()
+
 		# self.board[r][c]["highlightbackground"] = "#8EF0F7",
 
 	def display(self):
--- Mine_Button.py
+++ Mine_Button.py
@@ -4,12 +4,14 @@ import tkinter
 class Mine_Button(tkinter.Button):
 	val = None
 	hidden = None
+	locked = None
 
 	def __init__(self,ui):
 		tkinter.Button.__init__(self,ui)
 		self.hidden = True
+		locked = False
 
 	def show(self):
 		self["text"] = self.val
 		self.hidden = False
-		self["highlightbackground"] = "#8EF0F7"
+		# self["highlightbackground"] = "#8EF0F7"
